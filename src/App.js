2644f182ec0a77c374eed6e0bf00ed49c08acdb0
ryohei0109-develop <ryohei0109.develop@gmail.com>
2021-06-14 15:13:02 +0900

    #add stateによる画面表示変更処理実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // state定義
+    this.state = {name: '犬派'};
+  }
+  
+  handleClick(name) {
+    // stateの値を変更する際、this.state = {}; で直接代入はアンチパターン
+    this.setState({name: name});
+  }
+  
+  render() {
+    // return直下は1つのHTML要素のみ指定可能
+    return (
+    	<div>
+    	  <h1>私は、{this.state.name}です！</h1>
+        <button onClick={() => {this.handleClick('犬派')}}>犬派</button>
+        <button onClick={() => {this.handleClick('猫派')}}>猫派</button>
+      </div>
+    );
+  }
 }
 
 export default App;
